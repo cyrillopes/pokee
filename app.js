aecb73b85a1856fe8c14daba5bbff945b1d5995a
cyrillo1 <cyrillopez05@gmail.com>
2022-09-12 17:33:46 +0530

    heheh

--- app.js
+++ app.js
@@ -251,7 +251,7 @@ app.get("/orders", (req, res) => {
     });
 });
 
-app.put("/updateOrder/:id", (req, res) => {
+app.put("/orders/:id", (req, res) => {
   let oid = Number(req.params.id);
   db.collection("orders").updateOne(
     { orderId: oid },
